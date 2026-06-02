12a3d505badc2b7d36dd367f75e14a38910b0b1b
Jefferson Cordoba <jeffersoncordobap@gmail.com>
2026-06-02 17:20:19 -0500

    Configurar PostgreSQL para Render

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        default=config(
+            'DATABASE_URL',
+            default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        ),
+        conn_max_age=600
     )
 }
 
